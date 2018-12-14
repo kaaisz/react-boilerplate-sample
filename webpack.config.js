f0f0909df29333cdd63e2579e392557a7e36b47b
Kaai Suzuki <suzydp@me.com>
2018-12-14 02:13:14 -0800

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+
+  // If you set either:
+  // production - convert to optimised edition
+  // development - convert with generating JS file with Activating sourcemap
+  // mode: 'production',
+
+  // entry point - where is the file which do you wanna convert?
+  entry: {
+    app: './src/App.js',
+  },
+  // then, convert file from jsx to compatible scripts in any browsers
+  output: {
+    // "bundle" is for transpiling:
+    filename: 'build/bundle.js',
+    sourceMapFilename: 'build/bundle.map',
+  },
+  devtool: '#source-map',
+
+  module: {
+    loaders: [
+      {
+        // in case of .jsx - for react
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        // To declare using Babel 
+        loader: 'babel-loader',
+        // Specify options of Babel のオプションを指定する
+        options: {
+          presets: [
+            // Conver from ES2018 to ES5 by specifying presets
+            '@babel/preset-env',
+            // To identify JSX in React
+            '@babel/react'
+          ]
+        }
+      }
+    ]
+  }
+};
